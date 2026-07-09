cb9f05a1036645d091fdb2395f872071c918362f
narin-debug <skfls544@gmail.com>
2026-07-09 23:14:12 +0900

    Revert "Remove hero placeholder image box"
    This reverts commit 961277e8483dbab5763fe3410c07d176f93de6a6.

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,3 +1,4 @@
+import Placeholder from "@/components/Placeholder";
 import TextReveal from "@/components/TextReveal";
 import type { SiteContent } from "@/lib/content";
 
@@ -7,17 +8,20 @@ export default function Hero({ content }: { content: SiteContent }) {
       id="hero"
       className="relative flex h-screen w-full items-end overflow-hidden bg-background"
     >
-      {content.heroImageUrl && (
-        <>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={content.heroImageUrl}
-            alt={content.name}
-            className="absolute inset-0 h-full w-full object-cover"
-          />
-          <div className="absolute inset-0 bg-gradient-to-t from-background via-background/40 to-background/10" />
-        </>
+      {content.heroImageUrl ? (
+        // eslint-disable-next-line @next/next/no-img-element
+        <img
+          src={content.heroImageUrl}
+          alt={content.name}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+      ) : (
+        <Placeholder
+          label="[TODO] 히어로 배경 이미지 / 대표 편집 작업 스틸컷 (추후 영상 교체 가능)"
+          className="absolute inset-0 h-full w-full"
+        />
       )}
+      <div className="absolute inset-0 bg-gradient-to-t from-background via-background/40 to-background/10" />
 
       <div className="relative z-10 w-full px-6 pb-16 sm:px-10 sm:pb-20 md:px-14">
         <TextReveal
